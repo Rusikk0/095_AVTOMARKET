ce06566534ebc3012889ed2902b45178f8e877ea
Rusikk0 <rasyl6002@gmail.com>
2026-05-30 20:24:05 +0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Настройки Supabase
- * URL: Settings → Data API (без /rest/v1/)
- * Ключ: Settings → API Keys → Publishable key
- */
-window.AP_CONFIG = {
-  SUPABASE_URL: 'https://lkidrbdxvzqqboeowyfr.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_GXu6lH1Nbv1KnObqYp6EQQ_u9yt3hbX'
-};
